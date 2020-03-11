52a58443f3bde4168638bda8239ccbfbce49f6e4
Sekori1 <romaric55220@gmail.com>
2020-03-11 11:12:24 +0100

    Fix variable

--- index.php
+++ index.php
@@ -52,7 +52,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 
                 try {
                     $stmt = $conn->prepare("INSERT INTO LANGUE (NumLang, Lib1Lang, Lib2Lang, NumPays) VALUES (:NumLang, :Lib1Lang, :Lib2Lang, :NumPays)");
-                    $stmt->bindParam(':NumLang', $numSeqLang);
+                    $stmt->bindParam(':NumLang', $NumLang);
                     $stmt->bindParam(':Lib1Lang', $Lib1Lang);
                     $stmt->bindParam(':Lib2Lang', $Lib2Lang);
                     $stmt->bindParam(':NumPays', $numPays);
