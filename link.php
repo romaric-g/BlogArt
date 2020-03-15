d61bf3b418da49fada2eeef7ec58f9914b51bf8b
Sekori1 <romaric55220@gmail.com>
2020-03-15 18:42:16 +0100

    Fix mac

--- link.php
+++ link.php
@@ -1,14 +1,18 @@
 <?php
 
 function getLink($link) {  
-    $path = strtolower($_SERVER["CONTEXT_DOCUMENT_ROOT"]);
+    /* NE MARCHE PAS SUR MAC */
+    /*$path = strtolower($_SERVER["CONTEXT_DOCUMENT_ROOT"]);
     $link = strtolower("$link");
     $dir = strtolower(str_replace("\\", "/" , __DIR__));
 
     $root =  str_replace($path, "", $dir);
 
     $rootLink = $root  . $link;
-    return $rootLink;
+    return $rootLink;*/
+
+    return "/BlogArt" . $link;
+
 }
 
 ?>
