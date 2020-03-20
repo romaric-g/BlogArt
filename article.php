7ae48e5ba746da808184fd8003e7a7df5ea58c35
Sekori1 <romaric55220@gmail.com>
2020-03-20 20:01:47 +0100

    Change temoignage to article

---
+++ article.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="FR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="styles/css/common.css">
+    <link rel="stylesheet" href="styles/css/article.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+
+    </header>
+    <main>
+        <div class="container article">
+            
+         
+            <section class="head">
+                <h1 class="title">Témoignage : Un expert révèle le secret pour faire sa propre bière!</h1>
+                <p class="accroche"></p>
+                <p class="chapo">Et si vous appreniez à brasser votre propre bière ? Cette idée vous a surement déjà effleuré l’esprit si vous êtes amateur de cette boisson ambrée. Aujourd’hui, cela est possible ! Fabrice, notre expert, vous livre son secret !</p>
+            </section>
+
+            <section class="para">
+                <h2 class="stitre">Un stage de qualité dans une brasserie réputée</h2>
+                <p class="text">
+                    Il vous accueille deux samedis par mois entre 9h et 13h30 à la Brasserie Artisanale du Mascaret.
+                    Vous pourrez apprendre avec lui comment brasser votre bière de A à Z.
+                    Il faut savoir que n’importe qui peut s’inscrire à condition d’être intéressé par le sujet bien sûr : les débutants sont bienvenus, tout comme les plus expérimentés.
+                    Le stage commence par une heure trente de théorie pour comprendre le choix des ingrédients, le processus de brassage et l’élaboration de la recette. 
+                    Cela est suivi de deux heures trente de pratique. 
+                    Vous pourrez ensuite, en compagnie des douze autres participants, utiliser ce que vous avez précédemment appris pour la conception de vos pressions. 
+                    Ici encore, vous vous chargerez de toutes les étapes, de la recette à la mise en bouteille. Une fois terminé, Fabrice vous montrera un logiciel pour faire vos étiquettes. 
+                    En plus d’apprendre un véritable savoir-faire, vous pourrez passer une matinée dans la bonne humeur, entouré d’autres ayant la même passion que vous. C’est dans cette ambiance que le brasseur racontera ses nombreuses anecdotes et vous donnera ses précieux conseils. 
+                    Vous sortirez de cet atelier avec une quinzaine de bouteilles de 75cl, mais attention : il faudra attendre trois semaines avant de les consommer !
+                </p>
+            </section>
+
+            <section class="para">
+                <h2 class="stitre">Un stage de qualité dans une brasserie réputée</h2>
+                <p class="text">
+                    En interrogeant Fabrice, nous nous sommes rendu compte de l’expertise de cette enseigne. Lorsqu’on lui parle de sa brasserie, voilà ce qu’il nous répond : 
+                    “Depuis toujours notre brasserie artisanale est fière de ses 2 particularités qui la rendent unique : sa certification 100% BIO et son atelier de brassage amateur. 
+                    Dix ans après sa naissance, la brasserie comptabilise 19 récompenses pour l’ensemble de ses recettes. En 2019, elle a remporté la distinction suprême de la Meilleure Bière Blonde du Monde au concours WBA (World Beer Award) de Londres, et en 2020, elle a obtenu une Médaille d’Or au Concours Général Agricole. 
+                    L’entreprise emploie 8 salariés et recherche un développement en circuit court. Elle distribue à 98% en Nouvelle Aquitaine et à 90% en Gironde. Pour nous, la quête qualitative est un moteur à tous les niveaux de l’entreprise. 
+                    La brasserie propose une gamme de sodas dont un Cola BIO appelé Aloha Cola en référence aux amoureux du surf en général et de la glisse sur le Mascaret ! Nos bières sont très différentes les unes des autres et s'accordent à toutes les occasions. Nous avons très précisément 458 projets de recettes nouvelles pour vous surprendre au fil du temps ! 
+                    Pour la Saint Patrick la Brasserie lance une nouvelle recette « effet mer » pour une cervoise celte bio. Il s’agit d’une recette avec très peu de houblon ! Pour le remplacer, on a ajouté des épices fraîches, du gingembre et de la sauge. 
+                    Cela donne une boisson rafraîchissante douce, fraîche et sans amertume !”
+                </p>
+            </section>
+
+            <section class="para">
+                    <h2 class="stitre">Un succès écrasant les ambitions de la brasserie</h2>
+                    <p class="text">
+                        Comme le souligne Fabrice, “l’atelier bénéficie d’une excellente e-réputation”. Nous avions effectivement remarqué, en préambule, le nombre de liens parlant de la brasserie Mascaret en cherchant dans notre navigateur internet “Brassage bière bordeaux”. 
+                        Il existe plusieurs autres brasseries pratiquant ce type d’atelier sur la Belle Endormie, mais aucune d’entre elles ne semble rivaliser avec elle. Ce qui nous a surtout marqué, et la multitude de commentaires, d’avis et d’articles positifs. 
+                        Il en est de même sur la page Facebook de l’entreprise. Fabrice nous a également confié avoir reçu plus de 2000 stagiaires depuis la création de cet atelier. 
+                        Il nous dit aussi prendre “beaucoup de plaisir à réaliser ces stages” qui “vont continuer encore longtemps. Enfin, la brasserie Mascaret a répondu aux demandes des brasseurs expérimentés en leur proposant un autre atelier, plus avancé, pour ceux qui “aimeraient aller plus loin dans l’expérience du brassage de garage.” 
+                    </p>
+            </section>
+                
+            <section class="concl">
+                    <p class="text">
+                        Pour 132€, vous pourrez profiter de cette formation. Tout est fourni par la brasserie, apportez juste votre bonne humeur… et venez sans pression !
+                    </p>
+            </section>
+        </div>
+    </main>
+</body>
+</html>
