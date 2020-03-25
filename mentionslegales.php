98a6d3daa6845989b893d761f5f1158b819dea69
Sekori1 <romaric55220@gmail.com>
2020-03-25 13:09:48 +0100

    fix

--- mentionslegales.php
+++ mentionslegales.php
@@ -33,7 +33,7 @@ $user = User::getLoggedUser($conn);
 </head>
 <body>
   <?php PAGEHEADER($LANG, $user, $LANGUAGE, "./", $conn) ?>
-  <div class="container">
+  <div class="container mentions">
   <h2 class="title">Informations légales</h2>
     <h3 class="subtitle">1. Présentation du site</h3>
     <p>En vertu de l'article 6 de la loi n° 2004-575 du 21 juin 2004 pour la confiance dans l'économie numérique, il est précisé aux utilisateurs du site <a href="http://www.pressionbordelaise.com/">www.pressionbordelaise.com</a> l'identité des différents intervenants dans le cadre de sa réalisation et de son suivi :</p>
